e033048804d0a29a8f60ae7e0b43191669e19a78
chronialsgame <chronialsgame@gmail.com>
2025-03-13 13:46:49 +0530

    Update Banner.jsx

--- src/conponents/Banner.jsx
+++ src/conponents/Banner.jsx
@@ -7,13 +7,13 @@ import samDiana from '../assets/img/SamDiana.png';
         <section className="banner" id="home"> 
         <Container>
             <Row className="align-items-center">
-                <Col xs={12} md={6} xl={7}>
+                <Col xs={12} md={6} xl={6}>
                 <span className="tagline">Fight Cute, Play Fierce!</span>
                 <h1>{'Chronials'}<span className="wrap">!</span></h1>
                 <p>A battle field of the brave warriers protecting their land</p>
                 <button onClick={()=>console.log('connect')}>Let's Play<ArrowRightCircle size={25}/></button>
                 </Col>
-                <Col xs={12} md={6} xl={7}>
+                <Col xs={12} md={6} xl={6}>
                 <img src={samDiana} alt="Header img"/>
                 </Col>
             </Row>
